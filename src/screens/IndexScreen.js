5faf8cd3e99550b468bf05faf9666a1747222094
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-02-21 20:35:40 -0800

    Add ability to delete items by pressing on the trash icon

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text, FlatList, Button } from 'react-native';
+import { View, StyleSheet, Text, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
@@ -15,7 +15,10 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return <View style={styles.row}>
               <Text style={styles.title}>{item.title}</Text>
-              <Feather name="trash" style={styles.icon} />
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather name="trash" style={styles.icon} />
+              </TouchableOpacity>
+              
           </View>
         }}
       />
